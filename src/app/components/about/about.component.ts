cca004839f527534c1dcd44f68351c2716d9d49a
sebastian <sebastian.calleu@gmail.com>
2021-04-29 01:40:12 -0500

    using firebase json

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { InformacionService } from "../../services/informacion.service";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: []
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor( public _is:InformacionService) { }
 
 }
--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,12 +8,28 @@ export class InformacionService {
 
 	info:any = {};
 	cargada:boolean = false;
+	cargada_sobre_nosotros:boolean = false;
+	equipo:any = [];
 
   constructor( public http:HttpClient ) {
-	  this.http.get("assets/data/info.pagina.json")
-	  .subscribe( data =>{
-		  console.log(data);
-		  this.info = data;
-	  })
+	  this.carga_info();
+	  this.carga_sobre_nosotros();
+   }
+   public carga_info() {
+		this.http.get("assets/data/info.pagina.json")
+		.subscribe( data =>{
+			// console.log(data);
+			this.cargada = true;
+			this.info = data;
+	})
+   }
+
+   public carga_sobre_nosotros() {
+		this.http.get("https://paginaweb-46309-default-rtdb.firebaseio.com/equipo.json")
+		.subscribe( data =>{
+			console.log(data);
+			this.cargada_sobre_nosotros = true;
+			this.equipo = data;
+		})
    }
 }
