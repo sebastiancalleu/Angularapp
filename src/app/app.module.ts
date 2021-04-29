6b63705aa46e46f23b43aee5ebca997412a4a887
sebastian <sebastian.calleu@gmail.com>
2021-04-28 21:54:12 -0500

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// rutas
+
+import { app_routing } from "./app.routes";
+
+
+// components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export { AboutComponent } from "./about/about.component"
+export { PortafolioComponent } from "./portafolio/portafolio.component"
+export { ItemComponent } from "./item/item.component"
